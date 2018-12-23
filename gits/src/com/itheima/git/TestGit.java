6b0938a8c0663abfb2ed1bf6e05f7db27b8e0a3c
liuboqi <896287963@qq.com>
2018-12-23 19:38:01 +0800

    项目

---
+++ gits/src/com/itheima/git/TestGit.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("git!");
+        System.out.println("git!");
+        System.out.println("git!");
+        System.out.println("git!");
+        System.out.println("git!");
+        System.out.println("git!");
+        System.out.println("git!");
+        System.out.println("git!");
+        System.out.println("git!");
+    }
+}
